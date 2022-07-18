9d8e9c8e4b28772bdaec26c02d452c9f09122c01
Oleksandr Fediashov <olfedias@microsoft.com>
2022-07-18 13:07:35 +0200

    trying to reproduce

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { css } from '@linaria/core';
+
+import { mixin } from './mixins/styles';
 import icon from '../static/favicon.png';
 
 const header = css`
   text-transform: uppercase;
-  background-image: ${icon};
+  background-image: url(${icon});
+  
+  :hover {
+    ${mixin}
+  }
 `;
 
 console.log(header);
---
+++ src/mixins/styles.js
@@ -0,0 +1,5 @@
+import icon from '../../static/favicon.png';
+
+export const mixin = `
+  background-image: url(${icon});
+`;
